70436f69c5133a3ae4fec3bb1df8cdc6e6d8bb37
Oskar Szrajer <oskarszrajer@gmail.com>
2018-12-30 11:38:26 +0100

    basic static page skeleton

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source "https://rubygems.org"
+
+gem "builder"
+gem "dry-core"
+gem "dry-inflector"
+gem "dry-monads", "= 1.0.0.beta1"
+gem "dry-system"
+gem "dry-types"
+gem "dry-view"
+gem "pry"
+gem "slim"
+gem "transproc"
+
+group :development, :test do
+  gem "byebug"
+  gem "foreman"
+  gem "guard"
+  gem "guard-shell"
+end
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const ManifestPlugin = require("webpack-manifest-plugin")
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+var webpack = require('webpack')
+
+module.exports = function(env, argv) {
+  return {
+    entry: {
+      site: [
+        './assets/index.scss',
+      ],
+    },
+    output: {
+      filename: '[name].[hash:8].js',
+      path: __dirname + '/build/assets',
+    },
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true
+        }),
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    },
+    plugins: [
+      new ManifestPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "[name].[hash:8].css",
+        chunkFilename: "[id].css"
+      }),
+     new webpack.ProvidePlugin({
+       $: 'jquery',
+       jQuery: 'jquery'
+     }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[hash:8].[ext]',
+                useRelativePath: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  }
+}

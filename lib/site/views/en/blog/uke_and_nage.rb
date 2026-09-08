ebab1248131d29a76973b7836080bad9c26ebedb
gotar-deploy <deploy@users.noreply.github.com>
2026-09-08 02:08:50 +0200

    📝 blog: Uke i nage — relacja, która uczy (PL+EN) — poprawki po NO-GO t_091bcc75
    - Korekta językowa PL/EN (statysta, pojedynek, upadek, formy,
      zgodność liczby, nagłówek Relacja...); EN jako rodzeństwo-oryginał
      z dopisaną kodą (pierwsze zajęcia / błąd zaawansowanych).
    - R6: forma ustalona przez nauczyciela vs uczciwa intencja ataku;
      ochronne ukemi przed ambicją (lepiej paść za wcześnie niż za późno).
    - R4/R5: usunięte niezweryfikowane Breeland/Ikkyo i href=\"\";
      bibliografia tylko do materiałów POA, linki PL->PL, EN->EN.
    - Rejestracja: tytuł/opis/keywords SEO PL+EN, schema EN headline,
      odwrotna mapa EN->PL, usunięty martwy wpis seo_data;
      snapshot zregenerowany (162 ścieżki).
    Testy: 77 runs, 998 assertions, 0 failures. Build deterministyczny OK.

--- lib/site/views/en/blog/uke_and_nage.rb
+++ lib/site/views/en/blog/uke_and_nage.rb
@@ -3,14 +3,10 @@ require "site/import"
 
 module Site
   module Views
-    module En
-      module Blog
-        class UkeAndNage < View::Controller
-          configure do |config|
-            config.layout = "site_en"
-            config.template = "blog/uke-and-nage-a-learning-relationship_en"
-          end
-        end
+    class En::Blog::UkeAndNage < View::Controller
+      configure do |config|
+        config.layout = "site_en"
+        config.template = "blog/uke-and-nage-a-learning-relationship_en"
       end
     end
   end
